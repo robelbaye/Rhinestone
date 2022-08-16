750bfc40aca80bfc909feb3f34ef2a6e40379d99
Redet Getachew <redetg@gmail.com>
2022-08-16 22:35:00 +0300

    reset button for patent page

--- resources/views/components/forms/country/patent.blade.php
+++ resources/views/components/forms/country/patent.blade.php
@@ -37,11 +37,17 @@
                             <span class="material-icons" style="font-size: 15px;">print</span>
                         </a>
                     </td>
+
+                    <td>
+                        <button form="patent" type="reset"  class="btn btn-outline-primary btn-sm" href="#">
+                            <span class="material-icons" style="font-size: 15px;">backspace</span>
+                        </button>
+                    </td>
                 </tr>
             </table>
             <div class="card pt-3" style="margin-bottom: -80px;">
                 <div class="card-body row">
-                    <form class="row card-body" id="patent" action="{{ route('patent-crud.store') }}" method="POST">
+                    <form class="row card-body" name="patent" id="patent" action="{{ route('patent-crud.store') }}" method="POST">
                         @csrf
                         <div class="col-4">
                             <div class="form-group row">
