0e10d76f67fcba02ade3ebbb74a7c5f23a285187
Redet Getachew <redetg@gmail.com>
2022-08-21 10:45:03 +0300

    added a working table for design page

--- resources/views/portal/country/design.blade.php
+++ resources/views/portal/country/design.blade.php
@@ -138,26 +138,46 @@
             <div class="tab-pane fade" id="pills-Inventors" role="tabpanel" aria-labelledby="pills-Inventors-tab">
                 <div class="media">
                     <div class="table-responsive">
-                        <table class="table">
-                            <thead>
+                        <table id="Devrodesign" class="table" style="width:100%">
+                            <thead class="text-center">
                             <tr>
                                 <th>Case No</th>
                                 <th>Country</th>
                                 <th>Formal Title</th>
                                 <th>Type Of Filing</th>
                                 <th>Status</th>
+                                <th>Action</th>
                             </tr>
                             </thead>
-                            <tbody>
-                            <tr>
-                                <td>TEST-001</td>
-                                <td>TEST Surname</td>
-                                <td>TEST Given Name</td>
-                                <td>Ethiopian</td>
-                                <td>Active</td>
-                            </tr>
+                            <tbody class="text-center">
+                            @foreach ($designs as $desitem)
+                                <tr>
+                                    <td>{{$desitem->CaseNo}}</td>
+                                    <td>{{$desitem->Country}}</td>
+                                    <td>{{$desitem->FormalTitle}}</td>
+                                    <td>{{$desitem->TypeofFiling}}</td>
+                                    <td>{{$desitem->Status}}</td>
+                                    <td>
+                                        <a class="btn btn-outline-warning btn-sm" href="{{ url('/designcrud/' . $desitem->id . '/edit')}}">
+                                            <span class="material-icons" style="font-size: 15px;">edit</span>
+                                        </a>
+                                        <a class="btn btn-outline-secondary btn-sm" href="{{ url('/designcrud/' . $desitem->id . '/edit')}}">
+                                            <span class="material-icons" style="font-size: 15px;">copy_all</span>
+                                        </a>
+                                        <a class="btn btn-outline-primary btn-sm" href="#">
+                                            <span class="material-icons" style="font-size: 15px;">print</span>
+                                        </a>
+                                        <form method="POST" action="{{ url('/designcrud' . '/' . $desitem->id) }}" accept_charset="UTF-8" style="display:inline">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-outline-danger btn-sm" type="submit" title="Delete Patent" onclick="return confirm(&quot;Confirm to Delete ({{$desitem->CaseNo}}) ?&quot;)"><span class="material-icons" style="font-size: 15px;">delete</span></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
+
                     </div>
                 </div>
             </div>
